cda3a11d81b2313ee9fef653d45e99f77bcef8ef
harutiro <harutiro2027@outlook.jp>
2024-10-16 11:34:58 +0900

    add: おそらくセンサーを送る処理ができている

--- app/src/main/java/net/kajilab/elpissender/Service/SensingService.kt
+++ app/src/main/java/net/kajilab/elpissender/Service/SensingService.kt
@@ -1,6 +1,7 @@
 package net.kajilab.elpissender.Service
 
 import android.Manifest
+import android.app.Application
 import android.app.ForegroundServiceStartNotAllowedException
 import android.app.Notification
 import android.app.NotificationChannel
@@ -14,7 +15,17 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.ServiceCompat
 import androidx.core.content.PermissionChecker
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import net.kajilab.elpissender.API.FileExplorerApi
+import net.kajilab.elpissender.API.http.ApiResponse
 import net.kajilab.elpissender.R
+import net.kajilab.elpissender.Repository.SensingRepository
+import net.kajilab.elpissender.Repository.SensorBase
+import java.io.File
 
 class SensingService: Service() {
     override fun onBind(intent: Intent?): IBinder? {
@@ -28,7 +39,12 @@ class SensingService: Service() {
     }
 
     private fun sensing(){
-        Log.d("SensingService","sensing")
+        timerStart(
+            fileName = "background",
+            onStopped = {
+                Log.d("SensingService","BackGroundで一回実行されたよ")
+            }
+        )
     }
 
     private fun startForeground() {
@@ -74,5 +90,54 @@ class SensingService: Service() {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         return notificationBuilder.build()
+    }
+
+    var targetSensors: MutableList<SensorBase> = mutableListOf()
+    val apiResponse = ApiResponse(this)
+    val sensorRepository = SensingRepository(this)
+    var sensorStartFlag = false
+    private val serviceScope = CoroutineScope(Dispatchers.IO)
+
+    suspend fun start(fileName:String){
+
+        val samplingFrequency = -1.0
+
+        sensorRepository.sensorStart(
+            fileName = fileName,
+            sensors = targetSensors,
+            samplingFrequency = samplingFrequency
+        )
+        sensorStartFlag = true
+    }
+
+    fun stop(onStopped:() -> Unit){
+        sensorRepository.sensorStop(
+            sensors = targetSensors,
+            onStopped = { sensorFileList ->
+                val bleFile = sensorFileList[0]
+                val wifiFile = sensorFileList[1]
+
+                if(bleFile != null && wifiFile != null){
+                    apiResponse.postCsvData(wifiFile, bleFile)
+                }
+                onStopped()
+            }
+        )
+        sensorStartFlag = false
+        targetSensors = mutableListOf() // センサーをリセット
+    }
+
+    fun timerStart(fileName:String,onStopped:() -> Unit){
+        serviceScope.launch {
+            while(true){
+                start(fileName)
+                Log.d("Timer", "タイマー開始")
+                delay(10000)
+                Log.d("Timer", "タイマー終了")
+                stop(onStopped)
+                onStopped()
+                delay(10 * 60 * 1000)
+            }
+        }
     }
 }
