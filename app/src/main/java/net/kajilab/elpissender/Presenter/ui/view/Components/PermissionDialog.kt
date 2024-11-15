78c0472f105852b93984d15566ff9cd1930e66a0
harutiro <harutiro2027@outlook.jp>
2024-11-16 01:33:57 +0900

    add: パーミッションの許可ももらえるようになった。

--- app/src/main/java/net/kajilab/elpissender/Presenter/ui/view/Components/PermissionDialog.kt
+++ app/src/main/java/net/kajilab/elpissender/Presenter/ui/view/Components/PermissionDialog.kt
@@ -1,72 +1,143 @@
 package net.kajilab.elpissender.Presenter.ui.view.Components
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
+import android.provider.Settings
 import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
-
+import com.google.accompanist.permissions.shouldShowRationale
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun PermissionDialog() {
-    val permissionsState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    // フォアグラウンドパーミッション
+    val foregroundPermissionsState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         rememberMultiplePermissionsState(
             permissions = listOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.BLUETOOTH_SCAN,
                 android.Manifest.permission.BLUETOOTH_CONNECT,
                 android.Manifest.permission.BLUETOOTH_ADVERTISE,
                 android.Manifest.permission.BLUETOOTH_ADMIN,
-                android.Manifest.permission.INTERNET,
                 android.Manifest.permission.POST_NOTIFICATIONS
             )
         )
     } else {
         rememberMultiplePermissionsState(
             permissions = listOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
                 android.Manifest.permission.BLUETOOTH_ADMIN,
-                android.Manifest.permission.INTERNET,
             )
         )
     }
 
-    // パーミッションの状態に応じたUI
+    // バックグラウンドパーミッション
+    val backgroundPermissionState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        rememberPermissionState(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    } else {
+        null
+    }
+
+    // 状態に応じたログやダイアログの表示
     when {
-        permissionsState.allPermissionsGranted -> {
-            Log.d("LocationPermissionDialog", "allPermissionsGranted")
+        foregroundPermissionsState.allPermissionsGranted && (backgroundPermissionState?.status?.isGranted != false) -> {
+            Log.d("LocationPermissionDialog", "全てのパーミッションが許可されました")
         }
-        permissionsState.shouldShowRationale -> {
+        !foregroundPermissionsState.allPermissionsGranted -> {
             PermissionRationaleDialog(
-                onDismissRequest = { permissionsState.launchMultiplePermissionRequest() }
+                onDismissRequest = {
+                    activity.finish()
+                },
+                onConfirm = {
+                    foregroundPermissionsState.launchMultiplePermissionRequest()
+                }
             )
         }
+        foregroundPermissionsState.allPermissionsGranted && backgroundPermissionState?.status?.isGranted == false -> {
+            backgroundPermissionState.let {
+                if (!it.status.isGranted) {
+                    BackgroundPermissionRequestDialog(
+                        onDismissRequest = {
+                            activity.finish()
+                        },
+                        onOpenSettings = {
+                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", context.packageName, null)
+                            }
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+            }
+        }
         else -> {
-            Text("位置情報のパーミッションが拒否されました")
+            Log.d("LocationPermissionDialog", "パーミッションが拒否されました")
         }
     }
 }
 
 @Composable
-fun PermissionRationaleDialog(onDismissRequest: () -> Unit) {
+fun PermissionRationaleDialog(
+    onDismissRequest: () -> Unit,
+    onConfirm: () -> Unit = {}
+) {
     AlertDialog(
         onDismissRequest = onDismissRequest,
         title = { Text("このアプリケーションのパーミッションについて") },
         text = {
-            Text("アプリで正確な位置情報を取得するために位置情報へのアクセスを許可してください。")
-            Text("許可しない場合は、アプリの機能を利用できません。")
-            Text("また、通知の許可も必要です。")
+            Column {
+                Text("位置情報のパーミッションが必要です。")
+                Text("バックグラウンドで位置情報を取得するためには、フォアグラウンドパーミッションに加え、追加の許可が必要です。")
+            }
         },
         confirmButton = {
-            Button(onClick = onDismissRequest) {
+            Button(onClick = onConfirm) {
                 Text("許可する")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text("キャンセル")
+            }
+        }
+    )
+}
+
+@Composable
+fun BackgroundPermissionRequestDialog(
+    onDismissRequest: () -> Unit,
+    onOpenSettings: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = { /* 何もしない */ },
+        title = { Text("バックグラウンドパーミッションが必要です") },
+        text = {
+            Column {
+                Text("バックグラウンドでの位置情報の取得を許可する必要があります。")
+                Text("この許可は、アプリの設定画面で有効にすることもできます。")
+            }
+        },
+        confirmButton = {
+            Button(onClick = onOpenSettings) {
+                Text("設定画面を開く")
             }
         },
         dismissButton = {
